d683a42d5a7f3ff10fbb5c1d820094dbb4a28142
Xiangyang Ju <xiangyang.ju@gmail.com>
2023-01-19 22:42:02 -0800

    initial particle encoding

--- src/models/node_embedding_module.py
+++ src/models/node_embedding_module.py
@@ -6,8 +6,9 @@ from pytorch_lightning import LightningModule
 from scipy import stats
 from torchmetrics import MinMetric, MeanMetric
 
-class NodeEmbeddingModule(LightningModule):
-    """Metric Learning. Embedding nodes into a vector space."""
+class ParticleEmbeddingModule(LightningModule):
+    """Metric Learning. Embedding nodes into a vector space so that similar nodes are close together.
+    """
     def __init__(
         self,
         net: torch.nn.Module,
@@ -23,6 +24,23 @@ class NodeEmbeddingModule(LightningModule):
         self.train_loss = MeanMetric()
         self.val_loss = MeanMetric()
         self.test_loss = MeanMetric()
+
+        self.val_min_loss = MinMetric()
+        self.test_min_loss = MinMetric()
         
     def forward(self, x: torch.Tensor):
         return self.net(x)
+
+    def on_train_start(self):
+        self.val_min_loss.reset()
+        self.test_min_loss.reset()
+
+    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+
+        _, _, x_type_indices = batch
+        num_particles = x_type_indices.shape[1]
+        type_encoding = [self.net(x_type_indices[:, idx]) for idx in range(num_particles)]
+
+        ## true cases
+        
+
